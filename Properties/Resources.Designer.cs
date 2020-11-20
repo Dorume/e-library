4dbd1872f381542f0f2752365087942c5a90f894
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-20 12:43:13 +0200

    Создал форму для чтения/изменения текста.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace e_library.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Icon, аналогичного (Значок).
+        /// </summary>
+        internal static System.Drawing.Icon ReadIcon {
+            get {
+                object obj = ResourceManager.GetObject("ReadIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
