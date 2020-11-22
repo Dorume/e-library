4d538ad4d9ee479edfce2733b730f9f3ffd46856
Савицкий Даня <savitskiydanulo@gmail.com>
2020-11-22 21:35:04 +0200

    FIX ошибка при загрузке без файла .xml

--- Model/XMLFileHandler.cs
+++ Model/XMLFileHandler.cs
@@ -6,20 +6,26 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace e_library.Model
 {
     class XMLFileHandler : IFileHandler
     {
+
+        //Якщо вибрати FileMode.OpenOrCreate, то буде створений новий xml файл але
+        //він буде пустий, тому я відмовився від даного варіанта
         public TextFiles Load()
         {
             XmlSerializer xml = new XmlSerializer(typeof(TextFiles));
 
-            using (FileStream fs = new FileStream("TextFiles.xml", FileMode.OpenOrCreate))
+            using (FileStream fs = new FileStream("TextFiles.xml", FileMode.Open))
             {
-                return (TextFiles)xml.Deserialize(fs);
+                if (fs.CanRead)
+                    return (TextFiles)xml.Deserialize(fs);
             };
+            return null;
         }
 
         public void Save(TextFiles textFiles)
